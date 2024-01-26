ce77206e8520322724179c88a9b263b12767e760
ssomal62 <wxy890@gmail.com>
2024-01-26 11:41:46 +0900

    Initial commit with project files

---
+++ src/main/java/com/example/cherryweather/CherryWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.example.cherryweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CherryWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cherryweather/CherryWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cherryweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
